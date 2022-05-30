9790842e0289fbc5a7e7f88d9447415155d4ff98
winvale <harledvalencia01@gmail.com>
2022-05-30 15:29:28 -0500

    mensajes chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
   nuevoMensaje:string="";
-  constructor() { }
+  usuarioLogeado: any;
+  mensajes:any=[
+    {
+      emisor:"id",
+      texto:"Hola  AASsa "
+    },
+    {
+      emisor:"id",
+      texto:"Hola adasdasd "
+    },
+    {
+      emisor:"id",
+      texto:"Hola  asdasd asdas "
+    },
+    {
+      emisor:"id",
+      texto:"Hola asdasdasd asdasdasd "
+    },
+  ];
+    constructor(private authService:AuthService){
 
+    }
   ngOnInit(): void {
+      this.authService.getuserLogged().subscribe(usuario=>{
+        this.usuarioLogeado = usuario;
+      }
+        );
   }
   enviarMensaje(){
     console.log(this.nuevoMensaje);
+    this.nuevoMensaje="";
   }
 }
